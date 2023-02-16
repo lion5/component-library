ae3849bb40fe3b648fdbd7b3cfb61745217bb0da
Michael Götz <michael.goetz@lion5.io>
2023-02-16 16:59:16 +0100

    chore: generate docs pages for all registered components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,18 @@
-const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
-  },
-  "docs": {
-    "autodocs": "tag"
-  }
+    stories: [
+        "../src/**/*.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions"
+    ],
+    framework: {
+        name: "@storybook/vue3-vite",
+        options: {}
+    },
+    docs: {
+        autodocs: true
+    }
 }
