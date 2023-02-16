9cd82a3b47ee96ea49a971d83c7a2efbd3a120f5
Michael Götz <michael.goetz@lion5.io>
2023-02-16 16:22:22 +0100

    fix: CardDismissButton import path

--- src/atoms/buttons/CardDismissButton/CardDismissButton.spec.js
+++ src/atoms/buttons/CardDismissButton/CardDismissButton.spec.js
@@ -1,15 +1,12 @@
 import { mount } from '@vue/test-utils'
-import CardDismissButton from '@/base/components/card/CardDismissButton/CardDismissButton.vue'
 import { beforeEach, afterEach, describe, expect, it } from 'vitest'
+import CardDismissButton from "@/atoms/buttons/CardDismissButton/CardDismissButton.vue";
 
 
 describe('CardDismissButton', () => {
   let wrapper
   beforeEach(() => {
     wrapper = mount(CardDismissButton)
-  })
-  afterEach(() => {
-    wrapper.destroy()
   })
   describe('@events', () => {
     it('dismiss - is triggered on button click', async () => {
