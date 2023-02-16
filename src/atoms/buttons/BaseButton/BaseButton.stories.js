04ef0dc3973b8a5b816d472619d1c0169182c8c0
Michael Götz <michael.goetz@lion5.io>
2023-02-16 17:00:18 +0100

    chore: add base button

---
+++ src/atoms/buttons/BaseButton/BaseButton.stories.js
@@ -0,0 +1,67 @@
+import { action } from '@storybook/addon-actions'
+import BaseButton from "@/atoms/buttons/BaseButton/BaseButton.vue";
+import IconQRCode from "@/atoms/icons/IconQRCode.vue";
+
+export default {
+  components: {IconQRCode},
+  component: BaseButton,
+  argTypes: {}
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { BaseButton },
+  template: `
+    <BaseButton v-bind="$props" @click="click">
+      Slot Content
+    </BaseButton>`,
+  methods: {
+    click: action('click')
+  }
+})
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  variant: 'secondary'
+}
+
+export const Success = Template.bind({})
+Success.args = {
+  variant: 'success'
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  variant: 'danger'
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true
+}
+
+export const Loading = Template.bind({})
+Loading.args = {
+  loading: true
+}
+
+export const Icon = {
+  render: (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { BaseButton, IconQRCode },
+    template: `
+    <BaseButton v-bind="$props" @click="click">
+      <template #icon-left>
+        <IconQRCode />
+      </template>
+      Slot Content
+    </BaseButton>`,
+    methods: {
+      click: action('click')
+    }
+  }),
+};
+
--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -5,9 +5,11 @@ import BaseModal from "@/atoms/modals/BaseModal/BaseModal.vue";
 import DismissibleModal from "@/atoms/modals/DismissibleModal/DismissibleModal.vue";
 import CardDismissButton from "@/atoms/buttons/CardDismissButton/CardDismissButton.vue";
 import ItemCard from "@/atoms/cards/ItemCard/ItemCard.vue";
+import BaseButton from "@/atoms/buttons/BaseButton/BaseButton.vue";
 
 export {
     DraggableItem,
+    BaseButton,
     ActionButton,
     CardDismissButton,
     ItemCard,
