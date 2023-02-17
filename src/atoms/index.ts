a88fdb3eec6ff92bbb4941e8ee0fe492b322b5d9
Michael Götz <michael.goetz@lion5.io>
2023-02-17 18:00:54 +0100

    chore: add ApprovalModal

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -3,6 +3,7 @@ import ActionButton from "@/atoms/buttons/ActionButton/ActionButton.vue";
 import IconButton from "@/atoms/buttons/IconButton/IconButton.vue";
 import BaseModal from "@/atoms/modals/BaseModal/BaseModal.vue";
 import DismissibleModal from "@/atoms/modals/DismissibleModal/DismissibleModal.vue";
+import ApprovalModal from "@/atoms/modals/ApprovalModal/ApprovalModal.vue";
 import CardDismissButton from "@/atoms/buttons/CardDismissButton/CardDismissButton.vue";
 import ItemCard from "@/atoms/cards/ItemCard/ItemCard.vue";
 import BaseButton from "@/atoms/buttons/BaseButton/BaseButton.vue";
@@ -15,5 +16,6 @@ export {
     ItemCard,
     IconButton,
     BaseModal,
-    DismissibleModal
+    DismissibleModal,
+    ApprovalModal
 }
