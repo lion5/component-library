5396d7aa40b1bc4e0b022dfc3eac831e4b0456d0
Christopher Nagy <chrisn4gy@gmail.com>
2023-03-01 15:07:19 +0100

    docs: add SelectInput.stories.js

---
+++ src/atoms/inputs/SelectInput.stories.js
@@ -0,0 +1,29 @@
+import SelectInput from '@/atoms/inputs/SelectInput.vue'
+import { SelectOption } from '@/models/selectOption'
+
+export default {
+  components: { SelectInput },
+  component: SelectInput,
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: {
+    SelectInput,
+  },
+  setup() {
+    return { args }
+  },
+  template: '<SelectInput v-bind="args" />',
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  id: 'select-input',
+  label: 'Option',
+  placeholder: 'Choose option',
+  options: [
+    new SelectOption('option1', 'Option One'),
+    new SelectOption('option2', 'Option Two'),
+  ],
+}
