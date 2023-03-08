6cd61845a489b5fed6658389009fca45d3b4c019
Michael Götz <michael.goetz@lion5.io>
2023-03-08 16:07:52 +0100

    chore: implement v-model:modalDisplayed prop and event

--- src/atoms/modals/ApprovalModal/ApprovalModal.vue
+++ src/atoms/modals/ApprovalModal/ApprovalModal.vue
@@ -1,8 +1,8 @@
 <template>
   <slot :open="show" name="trigger" />
   <DismissibleModal
-      v-model:modal-displayed="modalDisplayed"
-      class="approval-modal"
+    v-model:modal-displayed="localModalDisplayed"
+    class="approval-modal"
   >
     <h2>{{ title }}</h2>
     <slot name="content"/>
@@ -20,26 +20,54 @@ import { ref } from 'vue'
 import DismissibleModal from "@/atoms/modals/DismissibleModal/DismissibleModal.vue";
 import BaseButton from "@/atoms/buttons/BaseButton/BaseButton.vue";
 
-withDefaults(
-    defineProps<{
-      title: string,
-      buttonLabel?: string
-    }>(),
-    { buttonLabel: 'Bestätigen' }
+const props = withDefaults(
+  defineProps<{
+    /**
+     * The title is displayed at the top of the modal
+     */
+    title: string
+    /**
+     * The button label of the approval button inside the modal
+     */
+    buttonLabel?: string
+    /**
+     * Open and closes modal (true => open, false => close)
+     * @model
+     */
+    modalDisplayed?: boolean
+  }>(),
+  {
+    modalDisplayed: false,
+    buttonLabel: 'Bestätigen',
+  }
 )
 
 const emit = defineEmits<{
   (e: 'accept'): void
   /**
+  /**
+   * True if modal should be displayed false if modal should be hidden
+   *
+   * @property {boolean} value the new modal state
    */
+  (event: 'update:modalDisplayed', value: boolean): void
+}>()
+
+const localModalDisplayed = computed({
+  get() {
+    return props.modalDisplayed
+  },
+  set(newShowModal) {
+    emit('update:modalDisplayed', newShowModal)
+  },
 })
 
 const accept = () => {
   emit('accept')
   modalDisplayed.value = false
+  localModalDisplayed.value = true
 }
 
-const modalDisplayed = ref<boolean>(false)
 </script>
 <style lang="scss" scoped>
 .approval-modal :deep(.content) {
