1e226df7adf6a45d9d49fe3e44ab172e55b2176f
Michael Götz <michael.goetz@lion5.io>
2023-05-22 14:28:30 +0200

    chore: add composables to index

---
+++ src/composables/index.ts
@@ -0,0 +1,23 @@
+import { useBrowserPermissions } from '@/composables/useBrowserPermissions'
+import { useDate } from '@/composables/useDate'
+import { useFetchState } from '@/composables/useFetchState'
+import { useFirebaseStorage } from '@/composables/useFirebaseStorage'
+import { useGeoJSON } from '@/composables/useGeoJSON'
+import { useInterval } from '@/composables/useInterval'
+import { useLeafletMap } from '@/composables/useLeafletMap'
+import { useNominatim } from '@/composables/useNominatim'
+import { useNumber } from '@/composables/useNumber'
+import { useShape } from '@/composables/useShape'
+
+export {
+  useBrowserPermissions,
+  useDate,
+  useFetchState,
+  useFirebaseStorage,
+  useGeoJSON,
+  useInterval,
+  useLeafletMap,
+  useNominatim,
+  useNumber,
+  useShape
+}
