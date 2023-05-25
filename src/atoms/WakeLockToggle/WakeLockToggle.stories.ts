31a8ab396dc59cdecadfdd0a75d31cdfcbf44737
Christopher Nagy <chrisn4gy@gmail.com>
2023-05-25 14:21:19 +0200

    feat: add WakeLockToggle.vue

---
+++ src/atoms/WakeLockToggle/WakeLockToggle.stories.ts
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/vue3'
+import WakeLockToggle from './WakeLockToggle.vue'
+import { createPinia, setActivePinia } from 'pinia'
+
+export default {
+  component: WakeLockToggle
+} as Meta<typeof WakeLockToggle>
+type Story = StoryObj<typeof WakeLockToggle>
+
+export const Default: Story = {
+  render: (args: unknown) => ({
+    components: { WakeLockToggle },
+    setup() {
+      setActivePinia(createPinia())
+      return { args }
+    },
+    template: `<WakeLockToggle v-bind='args' />`
+  })
+}
