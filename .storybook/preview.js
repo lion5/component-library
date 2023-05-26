178777738a0de95c763a368ab9f5210d6bbe5f20
Michael Götz <michael.goetz@lion5.io>
2023-05-26 10:11:05 +0200

    chore: add router to storybook vue to resolve RouterLink

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,23 @@
-import "./main.scss"
+import { setup } from '@storybook/vue3'
+import './main.scss'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+
+const routes = []
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+setup((app) => {
+  app.use(router)
+})
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
