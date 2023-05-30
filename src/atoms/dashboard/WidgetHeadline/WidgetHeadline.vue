ac6c15bdc1d1e93771f5bd4c1ee18b5d039f840f
Michael Götz <michael.goetz@lion5.io>
2023-05-30 14:29:01 +0200

    chore: add docs and tests to WidgetHeadline

--- src/atoms/dashboard/WidgetHeadline/WidgetHeadline.vue
+++ src/atoms/dashboard/WidgetHeadline/WidgetHeadline.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="widget-headline">
     <h2>{{ headline }}</h2>
-    <small>{{ subheading }}</small>
+    <small v-if="subheading">{{ subheading }}</small>
   </div>
 </template>
 <script lang="ts" setup>
 defineProps<{
   headline: string
-  subheading: string
+  subheading?: string
 }>()
 </script>
 <style lang="scss" scoped>
 .widget-headline {
   display: flex;
   flex-direction: column;
+
+  h2 {
+    font-size: var(--font-size-h2);
+    margin: 0;
+    line-height: 1.5;
+  }
+
+  small {
+    font-size: var(--font-size-0);
+  }
 }
 </style>
