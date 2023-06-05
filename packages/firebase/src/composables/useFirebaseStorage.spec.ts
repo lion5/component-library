2047600eb7f8afae4d2907dbbd9f3c97a70e9c14
Michael Götz <michael.goetz@lion5.io>
2023-06-05 14:42:46 +0200

    fix: lerna test run

---
+++ packages/firebase/src/composables/useFirebaseStorage.spec.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it, vi } from 'vitest'
+import { useFirebaseStorage } from '@/composables/useFirebaseStorage'
+import type {
+  FirebaseStorage,
+  StorageReference,
+  UploadResult
+} from 'firebase/storage'
+import { getStorage, ref, uploadBytes } from 'firebase/storage'
+
+vi.mock('firebase/storage')
+
+describe('useFirebaseStorage.ts', () => {
+  it('uploadFile - calls uploadBytes with given file and path', async () => {
+    const expectedFile = new File([], '', { type: 'image/png' })
+    const expectedFilePath = 'test-file-path'
+
+    const expectedStorage = 'fake-storage' as unknown as FirebaseStorage
+    const expectedRef = 'fake-storage-ref' as unknown as StorageReference
+    const expectedReturnPath = 'test-returned-path'
+
+    getStorage.mockReturnValue(expectedStorage)
+    ref.mockReturnValue(expectedRef)
+    uploadBytes.mockResolvedValueOnce({
+      ref: { fullPath: expectedReturnPath }
+    } as UploadResult)
+    const { uploadFile } = useFirebaseStorage()
+    await uploadFile(expectedFile, expectedFilePath)
+
+    expect(ref).toHaveBeenCalledWith(expectedStorage, expectedFilePath)
+    expect(uploadBytes).toHaveBeenCalledWith(expectedRef, expectedFile, {
+      contentType: expectedFile.type
+    })
+  })
+})
