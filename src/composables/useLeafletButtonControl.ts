a5aa0cefe5da9b5e33ca543cd23202c2fcdb34c8
Benedikt Erhart <contact@b-erhart.de>
2023-12-18 09:00:33 +0100

    chore: add callback hook to leaflet ButtonControl and enable state changes from outside

--- src/composables/useLeafletButtonControl.ts
+++ src/composables/useLeafletButtonControl.ts
@@ -1,9 +1,17 @@
-import { ref } from 'vue'
-import { ButtonControl } from '@/models/leafletButtonControl'
+import {Ref, ref} from 'vue'
+import {ButtonControl, StateChangeType} from '@/models/leafletButtonControl'
 
-export function useLeafletButtonControl(iconKey: string, tooltipTitle: string) {
+export function useLeafletButtonControl(
+  iconKey: string,
+  tooltipTitle: string,
+  stateChangeHook?: (
+    changedState: Ref<boolean>,
+    changeType: StateChangeType
+  ) => void
+) {
+
   const isActive = ref<boolean>(false)
-  const buttonControl = new ButtonControl(isActive, iconKey, tooltipTitle)
+  const buttonControl = new ButtonControl(isActive, iconKey, tooltipTitle, stateChangeHook)
 
   return { buttonControl, isActive }
 }
--- src/models/leafletButtonControl.ts
+++ src/models/leafletButtonControl.ts
@@ -1,17 +1,28 @@
 import { Control, DomEvent, DomUtil, Map } from 'leaflet'
 import type { Ref } from 'vue'
+import {watch} from "vue";
+
+export enum StateChangeType {
+    ACTIVATION,
+    DEACTIVATION,
+}
 
 export class ButtonControl extends Control {
   readonly state: Ref<boolean>
   readonly activeClass = 'active'
   readonly iconKey: string
   readonly tooltipTitle: string
+  readonly stateChangeHook?: (
+      changedState: Ref<boolean>,
+      changeType: StateChangeType
+  ) => void
 
-  constructor(state: Ref<boolean>, iconKey: string, tooltipTitle: string) {
+  constructor(state: Ref<boolean>, iconKey: string, tooltipTitle: string, stateChangeHook?: (changedState: Ref<boolean>, changeType: StateChangeType) => void) {
     super()
     this.state = state
     this.iconKey = iconKey
     this.tooltipTitle = tooltipTitle
+    this.stateChangeHook = stateChangeHook
   }
 
   onAdd(_map: Map) {
@@ -20,10 +31,26 @@ export class ButtonControl extends Control {
     button.innerHTML = `<i class='bi ${this.iconKey}'></i>`
     DomEvent.disableClickPropagation(button)
     DomEvent.on(button, 'click', () => {
-      this.state.value = button.classList.toggle(this.activeClass)
+        this.state.value = !this.state.value
     })
 
     container.title = this.tooltipTitle
+
+    watch(this.state, () => {
+      let changeType: StateChangeType
+
+      if (this.state.value) {
+        button.classList.add(this.activeClass)
+        changeType = StateChangeType.ACTIVATION
+      } else {
+        button.classList.remove(this.activeClass)
+        changeType = StateChangeType.DEACTIVATION
+      }
+
+      if (this.stateChangeHook) {
+        this.stateChangeHook(this.state, changeType)
+      }
+    })
 
     return container
   }
