21ca7c56b2eeee35cbde9ddd3c8dbd36c181920e
Michael Götz <michael.goetz@lion5.io>
2024-05-08 05:25:54 +0200

    chore: add tests and docs

--- packages/core/src/components/inputs/CurrencyInput/CurrencyInput.stories.ts
+++ packages/core/src/components/inputs/CurrencyInput/CurrencyInput.stories.ts
@@ -2,32 +2,36 @@ import { Meta, StoryObj } from '@storybook/vue3'
 import { Form } from 'vee-validate'
 import CurrencyInput from './CurrencyInput.vue'
 
+/**
+ * The CurrencyInput component is used to input a cent amount. The inserted value is formatted as euros.
+ *
+ * The design files can be found in [figma](https://www.figma.com/file/t7Sf0lcqLOFsWf9IfczzDf/Bamberg-Gutschein?type=design&node-id=2215%3A9872&mode=design&t=HMv5F0wNEzgDKZeY-1)
+ */
+
 export default {
   component: CurrencyInput
 } as Meta<typeof CurrencyInput>
 type Story = StoryObj<typeof CurrencyInput>
 
-export const TextInput: Story = {
+export const Empty: Story = {
+  args: {
+    name: 'test-empty'
+  }
+}
+
+export const Filled: Story = {
   render: (args: unknown) => ({
     components: { CurrencyInput, Form },
     setup() {
       return { args }
     },
     template: `
-      <Form :initialValues='{input1: "Text Input"}'>
+      <Form :initialValues='{ input1: 234 }'>
         <CurrencyInput v-bind='args' />
       </Form>`
   }),
   args: {
-    name: 'input1',
-    label: 'Text Input',
-    type: 'text'
-  }
-}
-
-export const Empty: Story = {
-  args: {
-    name: 'test-empty'
+    name: 'input1'
   }
 }
 
@@ -38,7 +42,7 @@ export const WithError: Story = {
       return { args }
     },
     template: `
-      <Form :initialValues='{"input-with-error": "Some wrong text"}'
+      <Form :initialValues='{"input-with-error": -100}'
             :initialTouched='{"input-with-error": true}'
             :validateOnMount='true'
             :initialErrors='{"input-with-error": "This is an error"}'
--- packages/core/src/components/inputs/CurrencyInput/CurrencyInput.vue
+++ packages/core/src/components/inputs/CurrencyInput/CurrencyInput.vue
@@ -1,15 +1,19 @@
 <template>
   <BaseInputWrapper>
     <input
-      ref="currencyInputField"
       :id="name"
+      :class="{
+        dirty: meta.dirty,
+        valid: meta.touched && meta.valid,
+        invalid: meta.touched && !meta.valid
+      }"
       :name="name"
       type="tel"
       :value="displayedCurrencyValue"
       placeholder="hidden"
       v-bind="$attrs"
-      @input="handleInput"
-      @keydown.delete="handleDelete"
+      @input="onRawInput"
+      @keydown.delete="onDelete"
       @blur="handleBlur"
     />
     <label :for="name">{{ label }}</label>
@@ -18,16 +22,12 @@
       <i class="bi bi-currency-euro"></i>
     </template>
   </BaseInputWrapper>
-  {{ value }}
 </template>
 <script setup lang="ts">
 import { ErrorMessage, useField } from 'vee-validate'
 import BaseInputWrapper from '@core/components/inputs/BaseInputWrapper/BaseInputWrapper.vue'
-import { ref } from 'vue'
-
-/**
- * See https://www.figma.com/file/t7Sf0lcqLOFsWf9IfczzDf/Bamberg-Gutschein?type=design&node-id=2215%3A9872&mode=design&t=HMv5F0wNEzgDKZeY-1
- */
+import { ref, watch } from 'vue'
+import { useCurrencyFormat } from '@core/components/inputs/CurrencyInput/useCurrencyFormat'
 
 const props = withDefaults(
   defineProps<{
@@ -39,18 +39,28 @@ const props = withDefaults(
      * The text to be displayed within a floating label of this field.
      */
     label?: string
+    /**
+     * The value of the input field. Mainly used for backwards compatibility to our old forms.
+     * Please use the vee validate form to fill this field instead.
+     */
+    cents?: number
   }>(),
   {
     label: 'Betrag'
   }
 )
 
-const INITIAL_VALUE_SIZE = 4
+/**
+ * The internal string representation of the number.
+ * Used to easily add and remove characters and to format the displayed value and the convert to the number value.
+ */
 const internalValue = ref<string>('')
-const currencyInputField = ref<HTMLInputElement | null>(null)
 const displayedCurrencyValue = ref<string>('00,00')
 
-const handleInput = (event: InputEvent) => {
+const onRawInput = (event: Event) => {
+  onInput(event as InputEvent)
+}
+const onInput = (event: InputEvent) => {
   if (event.data == null || event.data.match(/[0-9]/) == null) {
     ;(event.target as HTMLInputElement).value = displayedCurrencyValue.value
     return
@@ -59,42 +69,56 @@ const handleInput = (event: InputEvent) => {
     ;(event.target as HTMLInputElement).value = displayedCurrencyValue.value
     return
   }
-  handleAdd(event.data)
+  onAdd(event.data)
 }
 
-const fillWithZeros = (value: string) => {
-  const missingZeros = INITIAL_VALUE_SIZE - value.length
-  if (missingZeros <= 0) {
-    return value
-  }
-  return '0'.repeat(missingZeros) + value
-}
-
-const formatCurrency = (value: string) => {
-  const filledValue = fillWithZeros(value)
-  return filledValue.slice(0, -2) + ',' + filledValue.slice(-2)
-}
-
-const handleAdd = (inputValue: string) => {
+/**
+ * Adds the given input value to the internal value.
+ * Sets the displayed currency value to the formatted internal value.
+ * Sets the value of the field as a number.
+ *
+ * @param inputValue The value to be added to the internal value.
+ */
+const onAdd = (inputValue: string) => {
   internalValue.value = internalValue.value + inputValue
-  displayedCurrencyValue.value = formatCurrency(internalValue.value)
-  setValue(parseInt(internalValue.value))
+  value.value = parseInt(internalValue.value)
 }
 
-const handleDelete = () => {
+/**
+ * Deletes the last character of the internal value.
+ * If the internal value is empty, nothing happens.
+ * Sets the displayed currency value to the formatted internal value.
+ * Sets the value of the field as a number.
+ */
+const onDelete = () => {
   if (internalValue.value.length === 0) {
     return
   }
   internalValue.value = internalValue.value.slice(0, -1)
-  displayedCurrencyValue.value = formatCurrency(internalValue.value)
-  setValue(parseInt(internalValue.value || '0'))
+  value.value = parseInt(internalValue.value || '0')
 }
-const { value, setValue, handleBlur } = useField<number>(
-  props.name,
-  (value) => value < 0,
+
+const { value, handleBlur, meta } = useField<number>(
+  () => props.name,
+  (value) => value >= 0 || 'Der Betrag muss größer oder gleich 0 sein.',
   {
-    initialValue: 0
+    syncVModel: 'cents'
   }
+)
+
+watch(
+  value,
+  (newValue: number) => {
+    if (newValue == null) {
+      return
+    }
+    internalValue.value = newValue.toString()
+    const { toEuroStringFromCentString } = useCurrencyFormat()
+    displayedCurrencyValue.value = toEuroStringFromCentString(
+      internalValue.value
+    )
+  },
+  { immediate: true }
 )
 </script>
 <style scoped lang="scss"></style>
