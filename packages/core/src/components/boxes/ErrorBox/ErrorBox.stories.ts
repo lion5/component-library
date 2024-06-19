4e05d906c3564c8cbc94d079fb5d10686cf6c440
Michael Götz <michael.goetz@lion5.io>
2024-06-19 07:36:44 +0200

    docs(boxes): :memo: add information about auto hide functions of the box components

--- packages/core/src/components/boxes/ErrorBox/ErrorBox.stories.ts
+++ packages/core/src/components/boxes/ErrorBox/ErrorBox.stories.ts
@@ -1,13 +1,19 @@
 import ErrorBox from '../../boxes/ErrorBox/ErrorBox.vue'
 import { Meta, StoryObj } from '@storybook/vue3'
 
+/**
+ * The error box should be used to display global errors that are not tied to a specific input field.
+ * It can display a single error or multiple errors.
+ *
+ * **If both props (error and errors) are undefined or empty, the box will not be rendered!**
+ */
 export default {
   component: ErrorBox
 } as Meta<typeof ErrorBox>
 type Story = StoryObj<typeof ErrorBox>
 
 /**
- * If only one error is given. It displays the error inside the box as string.
+ * If only one error is given. It displays the error inside the box on a single line.
  */
 export const SingleError: Story = {
   args: {
@@ -15,6 +21,9 @@ export const SingleError: Story = {
   }
 }
 
+/**
+ * If multiple errors are given. It displays each error as a list item.
+ */
 export const MultipleErrors: Story = {
   args: {
     errors: [
