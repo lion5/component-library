0014c643efa2ecb8f1f3dedc464a8b0462bff16c
Michael Götz <michael.goetz@lion5.io>
2024-11-22 11:44:15 +0100

    chore: add iban input

---
+++ packages/core/src/components/inputs/IbanInput/IbanInput.vue
@@ -0,0 +1,67 @@
+<template>
+  <BaseInputWrapper>
+    <input
+      :id="name"
+      :class="{
+        dirty: meta.dirty,
+        valid: meta.touched && meta.valid,
+        invalid: meta.touched && !meta.valid
+      }"
+      :name="name"
+      type="text"
+      :value="displayedValue"
+      placeholder="hidden"
+      v-bind="$attrs"
+      @input="onRawInput"
+      @blur="handleBlur"
+    />
+    <label :for="name">{{ label }}</label>
+    <ErrorMessage class="error" :name="name" />
+  </BaseInputWrapper>
+</template>
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+import { useField, ErrorMessage } from 'vee-validate'
+import { useIbanUtils } from '@core/composables'
+import BaseInputWrapper from '@core/components/inputs/BaseInputWrapper/BaseInputWrapper.vue'
+
+const props = withDefaults(
+  defineProps<{
+    iban?: string
+    name: string
+    label?: string
+  }>(),
+  {
+    label: 'IBAN'
+  }
+)
+
+const { toFormattedIBAN, toRawIBAN, isValidIBAN } = useIbanUtils()
+const { value, handleBlur, meta } = useField<string>(
+  () => props.name,
+  (iban: string) =>
+    isValidIBAN(iban) || 'Bitte geben Sie eine gültige IBAN ein.',
+  {
+    syncVModel: 'iban'
+  }
+)
+const displayedValue = ref<string>('')
+
+const onRawInput = (event: Event) => {
+  onInput(event as InputEvent)
+}
+const onInput = (event: InputEvent) => {
+  const inputValue = (event.target as HTMLInputElement).value
+  value.value = toRawIBAN(inputValue)
+}
+
+watch(
+  value,
+  (iban) => {
+    displayedValue.value = toFormattedIBAN(iban)
+  },
+  {
+    immediate: true
+  }
+)
+</script>
