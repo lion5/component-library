b9b5906cbfcd601a8784ea8694e3a2ebe0642794
Michael Götz <michael.goetz@lion5.io>
2024-11-22 11:42:25 +0100

    chore: add version 3 input

---
+++ packages/core/src/components/inputs/BaseInputV3/BaseInputV3.vue
@@ -0,0 +1,206 @@
+<template>
+  <div
+    :class="{
+      'base-input-wrapper': true,
+      dirty,
+      invalid: invalid || errors.length > 0
+    }"
+  >
+    <div class="input-group">
+      <slot name="prefix" />
+      <div class="input-label">
+        <input
+          :id="name"
+          :name="name"
+          v-model.trim="value"
+          :type="type"
+          v-bind="$attrs"
+          placeholder="hidden"
+        />
+        <label :for="name">{{ label }}</label>
+      </div>
+      <div class="postfix">
+        <IconError v-if="invalid || errors.length > 0" class="error-icon" />
+        <slot v-else name="postfix" />
+      </div>
+    </div>
+    <ErrorBox class="error-box" :errors="errors" />
+  </div>
+</template>
+<script setup lang="ts">
+import IconError from '../../icons/IconError.vue'
+import { ErrorBox } from '@core/components'
+
+type InputValue = string | number | null
+
+withDefaults(
+  defineProps<{
+    /**
+     * Used to identify this field in a form (VeeValidate Form).
+     */
+    name: string
+    /**
+     * The text to be displayed within a floating label of this field.
+     */
+    label: string
+    /**
+     * The type of the HTML `input` element, e.g. "text", "password", or "number".
+     */
+    type?: string
+    /**
+     * Whether to show the error icon or not. Defaults to `true`.
+     */
+    showErrorIcon?: boolean
+    /**
+     * The meta information of the field. This is provided by `useField` from `vee-validate`.
+     */
+    dirty?: boolean
+    invalid?: boolean
+    /**
+     * The errors of the field. This is provided by `useField` from `vee-validate`.
+     */
+    errors?: Error[]
+  }>(),
+  {
+    type: 'text',
+    validationRules: '',
+    dirty: false,
+    invalid: false,
+    showErrorIcon: true,
+    errors: []
+  }
+)
+/**
+ * The value to display
+ */
+const value = defineModel<InputValue>()
+</script>
+<style scoped lang="scss">
+.base-input-wrapper {
+  --_input-size: var(--input-font-size, 1.2rem);
+  --_label-size: var(--input-label-font-size, 0.75rem);
+  --_input-surface-color: var(--input-surface-color, var(--color-neutral-200));
+  --_input-border-radius: var(--input-border-radius, var(--border-radius-300));
+  --_input-error-color: var(--color-danger);
+  --_input-error-color-hover: var(--color-danger-hover);
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-300);
+  width: 100%;
+  font-size: var(--_input-size);
+
+  .input-group {
+    display: flex;
+    border-radius: var(--_input-border-radius);
+    background-color: var(--_input-surface-color);
+    align-items: center;
+    padding-inline: var(--space-sm);
+    gap: var(--space-sm);
+
+    &:has(*:focus) {
+      outline: 2px solid var(--color-primary);
+    }
+  }
+
+  .input-label {
+    display: grid;
+    position: relative;
+    gap: var(--space-sm);
+    flex-grow: 1;
+
+    input,
+    label {
+      grid-row: 1 / 2;
+      grid-column: 1 / 2;
+      font-size: var(--_input-size);
+      background-color: transparent;
+    }
+
+    input {
+      border: none;
+      padding-block-end: var(--space-xs);
+      padding-block-start: calc(var(--_label-size) + var(--space-sm));
+
+      &:focus {
+        outline: none;
+      }
+
+      &[type='color'] {
+        -webkit-appearance: none;
+        border: none;
+        width: 100%;
+        height: 2.5rem;
+
+        &::-webkit-color-swatch-wrapper {
+          padding: 0;
+        }
+
+        &::-webkit-color-swatch {
+          border: none;
+          border-radius: var(--_input-border-radius);
+        }
+      }
+    }
+
+    label {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      color: var(--color-neutral-600);
+      transform-origin: left center;
+      transition: 0.15s ease-out;
+      transition-property: top, margin-top, transform, font-size;
+      cursor: text;
+      user-select: none;
+      line-height: 1;
+    }
+
+    input:not(:placeholder-shown) ~ label,
+    input:focus ~ label {
+      top: 0;
+      transform: translateY(0%);
+      margin-top: var(--space-xs);
+      font-size: var(--_label-size);
+    }
+  }
+
+  input::placeholder,
+  input::-webkit-input-placeholder {
+    color: transparent !important;
+    opacity: 0;
+  }
+
+  .error-icon,
+  .postfix {
+  }
+
+  .error-icon {
+    color: var(--color-danger);
+  }
+
+  &.invalid input ~ label {
+    color: var(--_input-error-color);
+  }
+
+  &.invalid input:hover ~ label {
+    color: var(--_input-error-color-hover);
+  }
+
+  &.invalid .input-group:has(input:not(:focus)) {
+    outline: 2px solid var(--_input-error-color);
+
+    &:hover {
+      outline: 2px solid var(--_input-error-color-hover);
+
+      .error-icon {
+        color: var(--_input-error-color-hover);
+      }
+    }
+  }
+
+  .error {
+    color: var(--_input-error-color);
+    font-size: var(--font-size-0);
+  }
+}
+</style>
