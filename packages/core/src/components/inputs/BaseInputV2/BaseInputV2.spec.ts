bd5543cf7a1605362dd718545f1a5fd2bcd7eed7
Michael Götz <michael.goetz@lion5.io>
2024-11-22 17:55:25 +0100

    chore(BaseInputV2): remove obsolete test

--- packages/core/src/components/inputs/BaseInputV2/BaseInputV2.spec.ts
+++ packages/core/src/components/inputs/BaseInputV2/BaseInputV2.spec.ts
@@ -27,15 +27,6 @@ describe('BaseInput.vue', () => {
       expect(wrapper.find('input').attributes('name')).toBe(expectedName)
       expect(wrapper.find('input').attributes('id')).toBe(expectedName)
     })
-    it(':name - is applied to ErrorMessage', async () => {
-      const expectedName = 'expectedFieldName'
-      await wrapper.setProps({ name: expectedName })
-      await wrapper.setProps({ modelValue: '' })
-
-      await flushPromises()
-
-      expect(wrapper.findComponent(ErrorMessage).vm.name).toBe(expectedName)
-    })
     it(':label - is rendered as label', async () => {
       const expectedLabel = 'Expected Label'
       await wrapper.setProps({ label: expectedLabel })
