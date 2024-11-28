0bd43388012a5bb976290669f9424becbfe1aff8
Michael Götz <michael.goetz@lion5.io>
2024-11-28 19:19:27 +0100

    chore(ImageGallery): convert to vue 3 and update test and docs

---
+++ packages/core/src/components/image/BaseImage/BaseImage.spec.ts
@@ -0,0 +1,32 @@
+import { mount } from '@vue/test-utils'
+import { beforeEach, describe, expect, it } from 'vitest'
+import type { defineComponent } from 'vue'
+import BaseImage from '@core/components/image/BaseImage/BaseImage.vue'
+import { MinimalImage } from '@core/components/image/models/minimalImage'
+
+describe('BaseImage.vue', () => {
+  let wrapper: ReturnType<typeof defineComponent>
+  let image: MinimalImage
+
+  beforeEach(() => {
+    image = new MinimalImage(
+      4711,
+      'testAltTag',
+      'url'
+    )
+    wrapper = mount(BaseImage, {
+      props: {
+        image
+      }
+    })
+  })
+
+  describe(':props', () => {
+    it(':image - is applied to img', async () => {
+      const imageElement = wrapper.find('img')
+
+      expect(imageElement.attributes('src')).toBe(image.url)
+      expect(imageElement.attributes('alt')).toBe(image.alt)
+    })
+  })
+})
