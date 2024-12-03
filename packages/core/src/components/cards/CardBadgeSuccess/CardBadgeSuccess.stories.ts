b3c4902a8a0f0a01fd4e9abc36f94af52f2e7861
Michael Götz <michael.goetz@lion5.io>
2024-12-03 14:50:16 +0100

    chore(CardBadgeSuccess): introduce new component

---
+++ packages/core/src/components/cards/CardBadgeSuccess/CardBadgeSuccess.stories.ts
@@ -0,0 +1,25 @@
+import CardBadgeSuccess from './CardBadgeSuccess.vue'
+import ItemCard from '../ItemCard/ItemCard.vue'
+import { Meta, StoryObj } from '@storybook/vue3'
+
+export default {
+  component: CardBadgeSuccess
+} as Meta<typeof CardBadgeSuccess>
+type Story = StoryObj<typeof CardBadgeSuccess>
+
+export const Default: Story = {
+  render: (args: unknown) => ({
+    components: { CardBadgeSuccess, ItemCard },
+    setup() {
+      return { args }
+    },
+    template: `
+      <ItemCard
+        style="display: grid;background-color: var(--color-primary-surface);aspect-ratio: 1/1;width: 200px;overflow: hidden;">
+        <CardBadgeSuccess v-bind="args">Badge slot text</CardBadgeSuccess>
+      </ItemCard>`
+  }),
+  args: {
+    tooltipText: 'This is a tooltip text',
+  }
+}
