b69acb501cf0b2851bd3fb3f52d15484ab9d87ba
Michael Götz <michael.goetz@lion5.io>
2025-01-27 17:16:25 +0100

    fix(CopyButton): match bg spec
    Fixes #212

--- packages/core/src/components/buttons/CopyButton/CopyButton.stories.ts
+++ packages/core/src/components/buttons/CopyButton/CopyButton.stories.ts
@@ -30,3 +30,13 @@ export const Example: Story = {
 export const HideIfNothingToCopy: Story = {
   args: {}
 }
+
+/**
+ * Displays a success message after copying the content. The message will be hidden after a short delay.
+ */
+export const ExtensiveSuccess: Story = {
+  args: {
+    ...Example.args,
+    extensiveSuccess: true
+  }
+}
