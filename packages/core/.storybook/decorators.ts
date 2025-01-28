c2d7d21b85a59520743ed7d0f9cd6c38e4fc8c06
Michael Götz <michael.goetz@lion5.io>
2025-01-28 13:23:43 +0100

    chore(AddressInput): improve address docs (use sb play function)

---
+++ packages/core/.storybook/decorators.ts
@@ -0,0 +1,30 @@
+import { action } from '@storybook/addon-actions'
+import { Form } from 'vee-validate'
+import { BaseButton } from '../src'
+
+export const formWrapper = (story: unknown, { args }) => {
+  return {
+    components: { story, Form, BaseButton },
+    setup() {
+      const onSubmit = action('submit')
+      return {
+        onSubmit,
+        initialValues: args.initialValues,
+        initialTouched: args.initialTouched,
+        validateOnMount: args.validateOnMount,
+        initialErrors: args.initialErrors
+      }
+    },
+    template: `
+      <Form
+        :initialValues='initialValues'
+        :initialTouched='initialTouched'
+        :validateOnMount='validateOnMount'
+        :initialErrors='initialErrors'
+        @submit="onSubmit"
+      >
+        <story />
+        <BaseButton type="submit">Submit</BaseButton>
+      </Form>`
+  }
+}
