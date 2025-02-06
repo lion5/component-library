f54bba60bda02cb0ce504ed28520501063a59994
Michael Götz <michael.goetz@lion5.io>
2025-02-06 20:19:48 +0100

    feat(PillList): introduce new component

---
+++ packages/core/src/components/display/PillList/PillList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+ /**
+ * A list of pills.
+ */
+import { Pill } from '@core/components/display/PillList/pill'
+ import BasePill from '@core/components/display/BasePill/BasePill.vue'
+
+defineProps<{
+  /**
+   * The pills to display.
+   */
+  pills: Pill[]
+}>()
+</script>
+
+<template>
+  <div class="pill-list">
+    <BasePill
+      v-for="pill in pills"
+      :key="pill.id"
+      :label="pill.label"
+      :icon-start-cls="pill.iconCls"
+      :busy="pill.busy"
+      :tooltip-text="pill.tooltipText"
+      :tooltip-show-on-hover="pill.tooltipShowOnHover"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.pill-list {
+  display: flex;
+  gap: var(--space-300);
+  flex-wrap: wrap;
+}
+</style>
