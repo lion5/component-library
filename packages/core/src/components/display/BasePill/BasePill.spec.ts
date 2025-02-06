7b632269a01ab543ed36bd6a21e1a6aef51c1c17
Michael Götz <michael.goetz@lion5.io>
2025-02-06 20:13:11 +0100

    feat(BasePill): integrate Tooltip into pill

--- packages/core/src/components/display/BasePill/BasePill.spec.ts
+++ packages/core/src/components/display/BasePill/BasePill.spec.ts
@@ -1,9 +1,9 @@
 import BasePill from './BasePill.vue'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
-import ArrowRotateLoadingAnimation from '@core/components/icons/ArrowRotateLoadingAnimation.vue'
-import { BaseIcon } from '@core/components'
 import { defineComponent } from 'vue'
+import BasePillContent from '@core/components/display/BasePill/BasePillContent.vue'
+import TooltipIcon from '@core/components/utils/TooltipIcon/TooltipIcon.vue'
 
 describe('BasePill.vue', () => {
   let wrapper: ReturnType<typeof defineComponent>
@@ -16,21 +16,31 @@ describe('BasePill.vue', () => {
     })
   })
   describe(':props', () => {
-    it(':busy - renders animation if true', async () => {
-      await wrapper.setProps({ busy: true })
-
-      expect(wrapper.findComponent(ArrowRotateLoadingAnimation).exists()).toBeTruthy()
-    })
-    it(':iconStartCls - rendered if set and not busy', async () => {
+    it(':label,:iconStartCls,:busy - are passed to BasePillContent', async () => {
+      const expectedLabel = 'Test Pill'
       const expectedIconCls = 'bi-person-fill'
-      await wrapper.setProps({ iconStartCls: expectedIconCls })
+      await wrapper.setProps({ busy: true, label: expectedLabel, iconStartCls: expectedIconCls })
 
-      expect(wrapper.findComponent(BaseIcon).props().icon).toBe(expectedIconCls)
+      expect(wrapper.findComponent(BasePillContent).props().label).toBe(expectedLabel)
+      expect(wrapper.findComponent(BasePillContent).props().iconStartCls).toBe(expectedIconCls)
+      expect(wrapper.findComponent(BasePillContent).props().busy).toBeTruthy()
     })
-    it(':iconStartCls - icon hidden if set and busy is set', async () => {
-      await wrapper.setProps({ iconStartCls: 'bi-person-fill', busy: true })
+    it(':tooltipText - Tooltip not rendered if not set', async () => {
+      await wrapper.setProps({ tooltipText: undefined })
 
-      expect(wrapper.findComponent(BaseIcon).exists()).toBeFalsy()
+      expect(wrapper.findComponent(TooltipIcon).exists()).toBeFalsy()
+    })
+    it(':tooltipText - Tooltip is rendered if set', async () => {
+      await wrapper.setProps({ tooltipText: 'Some text' })
+
+      expect(wrapper.findComponent(TooltipIcon).exists()).toBeTruthy()
+    })
+    it(':tooltipText,:tooltipShowOnHover - are passed to TooltipIcon', async () => {
+      const expectedTooltipText = 'Some text'
+      await wrapper.setProps({ tooltipText: expectedTooltipText, tooltipShowOnHover: false })
+
+      expect(wrapper.findComponent(TooltipIcon).props().tooltipText).toBe(expectedTooltipText)
+      expect(wrapper.findComponent(TooltipIcon).props().showOnHover).toBe(false)
     })
   })
 })
--- packages/core/src/components/display/BasePill/BasePill.vue
+++ packages/core/src/components/display/BasePill/BasePill.vue
@@ -1,47 +1,46 @@
 <script setup lang="ts">
-import BaseIcon from '@core/components/icons/BaseIcon.vue'
-import ArrowRotateLoadingAnimation from '@core/components/icons/ArrowRotateLoadingAnimation.vue'
+import BasePillContent from '@core/components/display/BasePill/BasePillContent.vue'
+import TooltipIcon from '@core/components/utils/TooltipIcon/TooltipIcon.vue'
 
-defineProps<{
-  label: string
-  iconStartCls?: string,
-  busy?: boolean
-}>()
+withDefaults(
+  defineProps<{
+    label: string
+    iconStartCls?: string,
+    busy?: boolean,
+    tooltipText?: string,
+    tooltipShowOnHover?: boolean
+  }>(),
+  {
+    iconStartCls: undefined,
+    busy: false,
+    tooltipText: undefined,
+    tooltipShowOnHover: true
+  }
+)
 </script>
 
 <template>
-  <div class="pill">
-    <span
-      v-if="busy"
-      class="start-icon"
-    >
-      <ArrowRotateLoadingAnimation />
-    </span>
-    <span
-      v-else-if="iconStartCls"
-      class="start-icon"
-    >
-      <BaseIcon :icon="iconStartCls" />
-    </span>
-    <span class="label">{{ label }}</span>
-    <slot name="postfix" />
-  </div>
+  <TooltipIcon
+    v-if="tooltipText"
+    :tooltip-text="tooltipText"
+    :show-on-hover="tooltipShowOnHover"
+  >
+    <template #tooltipTriggerContent>
+      <BasePillContent
+        :label="label"
+        :iconStartCls="iconStartCls"
+        :busy="busy"
+      />
+    </template>
+  </TooltipIcon>
+  <BasePillContent
+    v-else
+    :label="label"
+    :iconStartCls="iconStartCls"
+    :busy="busy"
+  />
+
 </template>
 
 <style scoped lang="scss">
-.pill {
-  display: flex;
-  align-items: center;
-  gap: var(--space-300);
-  padding: var(--space-300) var(--space-400);
-  width: max-content;
-  background-color: var(--pill-background-color, var(--color-primary));
-  color: var(--pill-color, var(--color-font-inverted));
-  border-radius: var(--border-radius-round);
-  line-height: 1;
-
-  .start-icon {
-    aspect-ratio: 1;
-  }
-}
 </style>
