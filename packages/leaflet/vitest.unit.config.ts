24d48b79aebeb4ee2730e1c570d4eccc528567b1
Michael Götz <mgoetz1995@gmail.com>
2025-06-17 14:32:41 +0200

    chore: fix linter issues

--- packages/leaflet/vitest.unit.config.ts
+++ packages/leaflet/vitest.unit.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
