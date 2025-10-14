9e28f7f283bd7247f105e0b1709c8fe9b07f140a
Michael Götz <michael.goetz@lion5.io>
2025-10-14 22:08:02 +0200

    chore(Button): start to implement a global button input

---
+++ packages/core/src/components/navigational/BaseButton/BaseLinkRouterLinkButton.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { RouteLocationRaw, RouterLink } from 'vue-router'
+import IconLoading from '@core/components/icons/IconLoading.vue'
+import { ButtonLevel, ButtonColor } from '@core/components/navigational/BaseButton/buttonVariants'
+
+const props = withDefaults(
+  defineProps<{
+    /**
+     * If set the button will be rendered as a link
+     */
+    href?: string
+    /**
+     * If set the button will be rendered as a router-link
+     */
+    to?: RouteLocationRaw
+    /**
+     * If true the button will be rendered as a label. This is useful for file inputs.
+     */
+    asLabel?: boolean
+  }>(),
+  {
+    asLabel: false
+  }
+)
+</script>
+
+<template>
+  <RouterLink
+    v-if="to"
+    :to="to"
+  >
+    <slot />
+  </RouterLink>
+  <a
+    v-else-if="href"
+    :href="href"
+  >
+    <slot />
+  </a>
+  <label
+    v-else-if="asLabel"
+  >
+    <slot />
+  </label>
+  <button
+    v-else
+    :type="type"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ packages/core/src/components/navigational/BaseButton/buttonVariants.ts
@@ -0,0 +1,14 @@
+export type ButtonColor =
+  | 'primary'
+  | 'neutral'
+  | 'info'
+  | 'warning'
+  | 'danger'
+  | 'success'
+  | 'outline-primary'
+  | 'outline-neutral'
+  | 'outline-success'
+  | 'outline-warning'
+  | 'outline-danger';
+
+export type ButtonLevel = 'primary' | 'secondary' | 'tertiary' | 'quaternary';
